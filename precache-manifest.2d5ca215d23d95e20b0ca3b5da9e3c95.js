92342d12a86f3058099d36a4273c7d5619e2fd52
Ola <elhazne@gmail.com>
2019-06-10 10:22:07 +0200

    Updates

---
+++ precache-manifest.2d5ca215d23d95e20b0ca3b5da9e3c95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cb009670d379b0d99dca8de96a2fe6",
+    "url": "/project__bestseller/index.html"
+  },
+  {
+    "revision": "377235c1bad4746fac74",
+    "url": "/project__bestseller/static/css/main.b8904cb0.chunk.css"
+  },
+  {
+    "revision": "b41f4832ab08a91d6a56",
+    "url": "/project__bestseller/static/js/2.17e13a4c.chunk.js"
+  },
+  {
+    "revision": "377235c1bad4746fac74",
+    "url": "/project__bestseller/static/js/main.46dd7b39.chunk.js"
+  },
+  {
+    "revision": "7c21d6535484b8c030c6",
+    "url": "/project__bestseller/static/js/runtime~main.9a0b41ce.js"
+  }
+]);
